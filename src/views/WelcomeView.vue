95fb4053e89e5ac9fb880441703852ffd9859198
YiGuMoYan <3194775246@qq.com>
2022-12-17 19:27:22 +0800

    WelcomeView ok

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="welcome">
+    <loading-component/>
+    <start-button-component/>
+  </div>
+</template>
+
+<script>
+
+import LoadingComponent from '@/components/LoadingComponent.vue'
+import StartButtonComponent from '@/components/StartButtonComponent.vue'
+
+export default {
+  name: 'WelcomeView',
+  components: { StartButtonComponent, LoadingComponent }
+}
+</script>
+
+<style scoped>
+.welcome {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
